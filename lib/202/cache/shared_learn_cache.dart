12b7a231497686bd3a035e837205f1ccbdd50027
its4thaxeon <its4thaxeon@outlook.com>
2022-10-19 23:57:07 +0300

    added a bit adjustment

---
+++ lib/202/cache/shared_learn_cache.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SharedLearn extends StatefulWidget {
+  const SharedLearn({super.key});
+
+  @override
+  State<SharedLearn> createState() => _SharedLearnState();
+}
+
+class _SharedLearnState extends State<SharedLearn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
