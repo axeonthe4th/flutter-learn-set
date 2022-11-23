5fc52ac6392c164539c66020105d01595e5ac0d7
its4thaxeon <its4thaxeon@outlook.com>
2022-11-24 00:22:39 +0300

    did more adjustment on feed_view

--- lib/303/feed_view.dart
+++ lib/303/feed_view.dart
@@ -14,6 +14,15 @@ class FeedView extends StatefulWidget {
 class _FeedViewState extends State<FeedView>
     with AutomaticKeepAliveClientMixin {
   final IPostService _postService = PostService();
+
+  late final Future<List<PostModel>?> _itemsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _itemsFuture = _postService.fetchPostItemsAdvanced();
+  }
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -23,36 +32,51 @@ class _FeedViewState extends State<FeedView>
         setState(() {});
       })),
       appBar: AppBar(),
-      body: FutureBuilder<List<PostModel>?>(
-          future: _postService.fetchPostItemsAdvanced(),
-          builder: ((BuildContext context,
-              AsyncSnapshot<List<PostModel>?> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-                return const Placeholder();
-
-              case ConnectionState.waiting:
-                return const Center(child: CircularProgressIndicator());
-              case ConnectionState.active:
-                return const Center(child: CircularProgressIndicator());
-              case ConnectionState.done:
-                if (snapshot.hasData) {
-                  return ListView.builder(
-                      itemCount: snapshot.data?.length ?? 0,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Card(
-                          child: ListTile(
-                              title: Text(snapshot.data?[index].body ?? "")),
-                        );
-                      });
-                } else {
-                  return const Placeholder();
-                }
-            }
-          })),
+      body: _FeedFutureBuilder(itemsFuture: _itemsFuture),
     );
   }
 
   @override
   bool get wantKeepAlive => true;
+}
+
+class _FeedFutureBuilder extends StatelessWidget {
+  const _FeedFutureBuilder({
+    Key? key,
+    required Future<List<PostModel>?> itemsFuture,
+  })  : _itemsFuture = itemsFuture,
+        super(key: key);
+
+  final Future<List<PostModel>?> _itemsFuture;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<PostModel>?>(
+        future: _itemsFuture,
+        builder:
+            ((BuildContext context, AsyncSnapshot<List<PostModel>?> snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return const Placeholder();
+
+            case ConnectionState.waiting:
+              return const Center(child: CircularProgressIndicator());
+            case ConnectionState.active:
+              return const Center(child: CircularProgressIndicator());
+            case ConnectionState.done:
+              if (snapshot.hasData) {
+                return ListView.builder(
+                    itemCount: snapshot.data?.length ?? 0,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Card(
+                        child: ListTile(
+                            title: Text(snapshot.data?[index].body ?? "")),
+                      );
+                    });
+              } else {
+                return const Placeholder();
+              }
+          }
+        }));
+  }
 }
